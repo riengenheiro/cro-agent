d4571bcadd519ad56e78e1e7fa96514b3b663652
Ricardo <engenheiroricardofarias@yahoo.com.br>
2026-03-06 09:16:00 -0300

    context loader do agente

--- worker/context-loader.js
+++ worker/context-loader.js
@@ -1,9 +1,11 @@
 import fs from "fs/promises"
 
 export async function loadProductBriefing(path) {
-  const content = await fs.readFile(path, "utf-8")
 
-  return {
-    raw: content
-  }
+    const content = await fs.readFile(
+        `../${path}`,
+        "utf-8"
+    )
+
+    return content
 }
---
+++ worker/sites-loader.js
@@ -0,0 +1,11 @@
+import fs from "fs/promises"
+
+export async function loadSites() {
+
+    const config = await fs.readFile(
+        "../sites.config.json",
+        "utf-8"
+    )
+
+    return JSON.parse(config)
+}
